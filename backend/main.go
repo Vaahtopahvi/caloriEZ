99f926aee882ec8277d0dc8c87a4dd40e1eaa596
Kimble23 <bibi.honkaniemi@gmail.com>
2026-02-07 14:26:46 +0200

    Add tdee calculation based on activity level

--- backend/main.go
+++ backend/main.go
@@ -14,14 +14,16 @@ func calculator(w http.ResponseWriter, r *http.Request) {
 	heightRaw := r.URL.Query().Get("height")
 	ageRaw := r.URL.Query().Get("age")
 	gender := r.URL.Query().Get("gender")
+	activity := r.URL.Query().Get("activity")
 
 	// Converts that text to deciam numbers
 	weight, _ := strconv.ParseFloat(weightRaw, 64)
 	height, _ := strconv.ParseFloat(heightRaw, 64)
 	age, _ := strconv.ParseFloat(ageRaw, 64)
 
+	// Variable for Basal Metabolic Rate
 	var bmr float64
-
+	// Calculates BMR
 	if gender == "male" {
 		// male formula: 10 x weight + 6.25 x height - 5 x age + 5
 		bmr = (10 * weight) + (6.25 * height) - (5 * age) + 5
@@ -29,15 +31,30 @@ func calculator(w http.ResponseWriter, r *http.Request) {
 		bmr = (10 * weight) + (6.25 * height) - (5 * age) - 161
 	}
 
+	// Variable for activity level multiplier
+	var multiplier float64
+	switch activity {
+	case "sedentary":
+		multiplier = 1.2
+	case "light":
+		multiplier = 1.375
+	case "moderate":
+		multiplier = 1.55
+	case "active":
+		multiplier = 1.725
+	default:
+		multiplier = 1.2
+	}
+
+	// Multiply bmr calories with activity level
+	tdee := bmr * multiplier
+
 	// Sends the answer back to user's browser
-	fmt.Fprintf(w, "Results: Your daily BMR is %.0f calories", bmr)
+	fmt.Fprintf(w, "Your BMR is %.0f. With your activity level (%s), your total daily burn is %.0f calories.", bmr, activity, tdee)
 }
 
 func main() {
-	// Connects URL path with calculator function
 	http.HandleFunc("/calculate", calculator)
-
-	// This is running the program
 	fmt.Println("Server starting on http://localhost:8080")
 	http.ListenAndServe(":8080", nil)
 }
