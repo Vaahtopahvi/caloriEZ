ada517ecb661a7a0561681e81112de347ad3d39a
riku <kermakahvi@hotmail.com>
2026-02-09 23:52:06 +0200

    import types and turn parameters into new URLSearchParams. create function to make an API call with fresh url

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,27 +1,26 @@
 // api calls
 
+import type { CalculatorInput, CalculatorResult } from "../types/calculator";
+
 // import API url from env variable or default to localhost
 const API_URL = import.meta.env.VITE_API_URL || "http://localhost:8080";
 
 // function to call the calculate endpoint with the parameters
-export const calculateCalories = async (
-  weight: number,
-  height: number,
-  age: number,
-  gender: string,
-  activity: string,
-) => {
-  // turn the parameters into a query string
+export const CalculateCalories = async (
+  // make sure the input is type of calculatorinput and the output will be a promise of calculatorresult
+  input: CalculatorInput,
+): Promise<CalculatorResult> => {
+  // turn the parameters into a query string with URLSearchParams (weight=70&height=175& etc...)
   const params = new URLSearchParams({
-    weight: weight.toString(),
-    height: height.toString(),
-    age: age.toString(),
-    gender,
-    activity,
+    weight: input.weight.toString(),
+    height: input.height.toString(),
+    age: input.age.toString(),
+    gender: input.gender,
+    activity: input.activity,
   });
 
   // call the API with parameters and return the response
   const response = await fetch(`${API_URL}/calculate?${params.toString()}`);
-  const data = await response.json();
+  const data: CalculatorResult = await response.json();
   return data;
 };
